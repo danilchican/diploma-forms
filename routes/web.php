6356774130944adafa0960dba2f3d0748f956688
danilchican <danilchican@mail.ru>
2019-04-17 17:30:10 +0300

    Added admin access middleware for the dashboard page.

--- routes/web.php
+++ routes/web.php
@@ -24,8 +24,8 @@ Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail'
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
 
-
-Route::group(['prefix' => 'dashboard', 'middleware' => ['auth']], function () {
+/* Dashboard Routes */
+Route::group(['prefix' => 'dashboard', 'middleware' => ['auth.access:admin']], function () {
     Route::get('/', 'Dashboard\DashboardController')->name('dashboard.home');
 });
 
