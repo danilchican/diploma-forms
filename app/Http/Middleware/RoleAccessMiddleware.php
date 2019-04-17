a3ab7ceffa71008694651d7ccfd3b65b94a46447
danilchican <danilchican@mail.ru>
2019-04-17 15:03:57 +0300

    Created role access middleware.

---
+++ app/Http/Middleware/RoleAccessMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Response;
+
+class RoleAccessMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure                 $next
+     *
+     * @param array                     $role
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next, ...$role)
+    {
+        if (\Auth::guest() || !$request->user()->hasRole($role)) {
+            return abort(Response::HTTP_NOT_FOUND);
+        }
+
+        return $next($request);
+    }
+}
