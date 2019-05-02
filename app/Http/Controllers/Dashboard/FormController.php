6c6343a805e37e6a82f3b4adc829a5fc799a69bf
danilchican <danilchican@mail.ru>
2019-05-02 23:40:14 +0300

    Created method handler to create new form. Added form request

--- app/Http/Controllers/Dashboard/FormController.php
+++ app/Http/Controllers/Dashboard/FormController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers\Dashboard;
 
+use App\Exceptions\CreateFormException;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Form\CreateFormRequest;
 use App\Models\AnswerType;
+use App\Models\AnswerVariant;
 use App\Models\Form;
+use App\Models\FormQuestion;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class FormController extends Controller
 {
@@ -17,5 +24,88 @@ class FormController extends Controller
     {
         $forms = Form::with('author')->paginate();
         return view('dashboard.forms.index')->with('forms', $forms);
+    }
+
+    /**
+     * Create new Form.
+     *
+     * @param CreateFormRequest $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function storeForm(CreateFormRequest $request)
+    {
+        try {
+            DB::beginTransaction();
+            $this->storeFormExecutor($request);
+            DB::commit();
+
+            return response()->json([
+                'success'  => true,
+                'messages' => ['Опрос успешно создан.'],
+            ]);
+        } catch (CreateFormException $e) {
+            DB::rollBack();
+
+            \Log::error($e->getMessage());
+            \Log::error($e->getTraceAsString());
+
+            return response()->json([
+                'success' => false,
+                'errors'  => [$e->getMessage()],
+            ]);
+        }
+    }
+
+    /**
+     * Store Form Executor.
+     *
+     * @param Request $request
+     *
+     * @throws CreateFormException
+     */
+    protected function storeFormExecutor(Request $request)
+    {
+        $user = \Auth::user();
+
+        $form = new Form($request->only(['title', 'description']));
+        $user->forms()->save($form);
+
+        $questions = $request->input('questions');
+        $questionModelsAndAnswers = collect();
+
+        foreach ($questions as $question) {
+            $questionModel = null;
+
+            try {
+                $answerType = AnswerType::named($question['answer_type'])->firstOrFail();
+            } catch (ModelNotFoundException $e) {
+                throw new CreateFormException('Выбран неверный тип ответа.');
+            }
+
+            $questionModel = new FormQuestion([
+                'title'       => $question['title'],
+                'is_required' => $question['is_required'],
+            ]);
+
+            $questionAnswers = collect();
+
+            if (array_key_exists('answers', $question) && \is_array($question['answers'])) {
+                foreach ($question['answers'] as $answer) {
+                    $questionAnswers->push(new AnswerVariant(['title' => $answer]));
+                }
+            }
+
+            $questionModel->answerType()->associate($answerType);
+            $questionModelsAndAnswers->push(['question' => $questionModel, 'answers' => $questionAnswers]);
+        }
+
+        $form->questions()->saveMany($questionModelsAndAnswers->pluck('question'));
+
+        $questionModelsAndAnswers->filter(function ($question) {
+            return array_key_exists('answers', $question) && collect($question['answers'])->isNotEmpty();
+        })->each(function ($question) {
+            $question['question']->answers()->saveMany($question['answers']);
+        });
     }
 }
---
+++ app/Http/Requests/Form/CreateFormRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests\Form;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title'                   => 'required|min:3|max:255',
+            'description'             => 'nullable|min:3|max:1000',
+            'questions'               => 'required|array|min:1',
+            'questions.*'             => 'array',
+            'questions.*.title'       => 'required|min:3|max:255',
+            'questions.*.answer_type' => 'required|exists:answer_types,type',
+            'questions.*.is_required' => 'required|boolean',
+            'questions.*.answers'     => 'required_if:questions.*.answer_type,radio,checkbox,select|array',
+            'questions.*.answers.*'   => 'required|min:1|max:255',
+        ];
+    }
+}
