79220e3fbe04f0fd71c6d409c51aafdd45e8be2c
danilchican <danilchican@mail.ru>
2019-05-04 12:41:16 +0300

    Created exception and requests to update form.

--- app/Http/Requests/Form/UpdateFormRequest.php
+++ app/Http/Requests/Form/UpdateFormRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\Form;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class CreateFormRequest extends FormRequest
+class UpdateFormRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,14 +24,17 @@ class CreateFormRequest extends FormRequest
     public function rules()
     {
         return [
+            'id'                             => 'required|integer|exists:forms',
             'title'                          => 'required|min:3|max:255',
             'description'                    => 'nullable|min:3|max:1000',
             'questions'                      => 'required|array|min:1',
             'questions.*'                    => 'array',
+            'questions.*.id'                 => 'nullable|integer|exists:form_questions,id',
             'questions.*.title'              => 'required|min:3|max:255',
             'questions.*.selectedAnswerType' => 'required|exists:answer_types,type',
             'questions.*.is_required'        => 'required|boolean',
             'questions.*.answers'            => 'required_if:questions.*.answer_type,radio,checkbox,select|array',
+            'questions.*.answers.*.id'       => 'nullable|integer|exists:answer_variants,id',
             'questions.*.answers.*.title'    => 'required|min:1|max:255',
         ];
     }
