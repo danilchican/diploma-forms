9427a580cdfe1d28b9102e947bb053b79a3d26a4
danilchican <danilchican@mail.ru>
2019-05-07 19:29:12 +0300

    Added text field for forms.

--- resources/views/partials/forms/view/text.blade.php
+++ resources/views/partials/forms/view/text.blade.php
@@ -1,1 +1,6 @@
-text
+<div class="form-group">
+    <div class="col-md-12 col-sm-12 col-xs-12">
+        <input placeholder="Введите ответ" class="form-control"
+               @if($question->isRequired()) required="required" @endif>
+    </div>
+</div>
--- resources/views/partials/forms/view/textarea.blade.php
+++ resources/views/partials/forms/view/textarea.blade.php
@@ -1,1 +1,6 @@
-textarea
+<div class="form-group">
+    <div class="col-md-12 col-sm-12 col-xs-12">
+        <textarea placeholder="Введите ответ" class="form-control" rows="4"
+                  @if($question->isRequired()) required="required" @endif></textarea>
+    </div>
+</div>
