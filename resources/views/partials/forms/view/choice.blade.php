e81004e7805df0a38b1707d8becf3f2493f16f4e
danilchican <danilchican@mail.ru>
2019-05-07 19:44:28 +0300

    Added choicable template.

--- resources/views/partials/forms/view/choice.blade.php
+++ resources/views/partials/forms/view/choice.blade.php
@@ -1,1 +1,27 @@
-{{ $question->answerType->type }}
+<label for="question-{{ $question->id }}">
+    @if($question->answerType->type !== 'checkbox')
+        Выберите вариант ответа:
+    @else
+        Выберите несколько вариантов ответа:
+    @endif
+</label>
+
+@if($question->answerType->type === 'select')
+    <select id="question-{{ $question->id }}" name="answers[{{ $question->id }}]" class="form-control">
+        @foreach($question->answers as $answer)
+            <option value="{{ $answer->id }}">{{ $answer->getTitle() }}</option>
+        @endforeach
+    </select>
+@else
+    @foreach($question->answers as $answer)
+        <div class="form-group">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <label for="answer-{{ $answer->id }}">
+                    <input id="answer-{{ $answer->id }}" type="{{ $question->answerType->type }}"
+                           name="answers[{{ $question->id }}][]"
+                           @if($question->isRequired()) required="required" @endif> {{ $answer->getTitle() }}
+                </label>
+            </div>
+        </div>
+    @endforeach
+@endif
