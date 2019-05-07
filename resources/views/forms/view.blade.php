2db3e9b35b91b16db9af25d50cb8d30581ed8c47
danilchican <danilchican@mail.ru>
2019-05-07 19:48:36 +0300

    Fixed view form template.

--- resources/views/forms/view.blade.php
+++ resources/views/forms/view.blade.php
@@ -6,31 +6,31 @@
     <p>{{ $form->getDescription() }}</p>
     <hr>
     {{--TODO add action--}}
-    <form action="#" method="POST" id="add-form-vote">
-        @foreach($questions->chunk(3) as $questionsChunk)
-            <div class="row">
-                @foreach($questionsChunk as $question)
-                    <div class="col-md-6">
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                {{ $loop->iteration . '. ' . $question->getTitle() }}
-                                @if($question->isRequired())
-                                    <span class="required" style="color: red;">*</span>
-                                @endif
-                            </div>
-                            <div class="panel-body">
-                                @if($question->isNeedToChooseAnswer())
-                                    @include('partials.forms.view.choice')
-                                @else
-                                    @include('partials.forms.view.' . $question->answerType->type)
-                                @endif
-                            </div>
+    <div class="col-md-8 col-md-offset-2">
+        <form action="#" method="POST" id="add-form-vote">
+            @foreach($questions as $question)
+                <div class="col-md-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            {{ $loop->iteration . '. ' . $question->getTitle() }}
+                            @if($question->isRequired())
+                                <span class="required" style="color: red;">*</span>
+                            @endif
+                        </div>
+                        <div class="panel-body">
+                            @if($question->isNeedToChooseAnswer())
+                                @include('partials.forms.view.choice')
+                            @else
+                                @include('partials.forms.view.' . $question->answerType->type)
+                            @endif
                         </div>
                     </div>
-                @endforeach
-            </div>
-        @endforeach
+                </div>
+            @endforeach
 
-        <button type="submit" class="btn btn-primary">Проголосовать</button>
-    </form>
+            <div class="col-md-12">
+                <button type="submit" class="btn btn-primary">Проголосовать</button>
+            </div>
+        </form>
+    </div>
 @endsection
