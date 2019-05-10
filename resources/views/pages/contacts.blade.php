c04812db15728f963631a39e032b1ae58ab7cb43
danilchican <danilchican@mail.ru>
2019-05-10 23:51:32 +0300

    Improved about and contacts page.

--- resources/views/pages/contacts.blade.php
+++ resources/views/pages/contacts.blade.php
@@ -4,4 +4,23 @@
     <div class="page-header">
         <h1>Контакты</h1>
     </div>
+    <div class="container">
+        <div class="col-md-7">
+            <h3>Место расположения преподавательских комнат:</h3>
+            <h4>805-5, 807-5, по адресу: Минск, Платонова, 39.</h4>
+
+            <h3>Контактные телефоны:</h3>
+            <h4>+(375 17)293-86-46; +(375 17)293-88-2</h4>
+
+            <h3>Почтовый индекс:</h3>
+            <h4>220013, Минск, П. Бровки, 6</h4>
+
+            <h3>Электронная почта:</h3>
+            <h4><a href="mailto:kafman@bsuir.by">kafman@bsuir.by</a></h4>
+        </div>
+        <div class="col-md-5">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2350.1326357397747!2d27.593702715340065!3d53.91161893992092!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dbcfaef3fae639%3A0x4876fd96cac1590!2z0YPQuy4g0J_Qu9Cw0YLQvtC90L7QstCwIDM5LCDQnNC40L3RgdC6!5e0!3m2!1sru!2sby!4v1557521428492!5m2!1sru!2sby"
+                    width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+    </div>
 @endsection
