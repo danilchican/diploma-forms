719eb73fea94a5f006526494be9b522aab9403a7
danilchican <danilchican@mail.ru>
2019-05-11 14:54:36 +0300

    Fixed controller to download statistics as pdf.

--- app/Http/Controllers/FormResultsController.php
+++ app/Http/Controllers/FormResultsController.php
@@ -12,8 +12,10 @@ class FormResultsController extends Controller
     {
         $relations = $this->getStatisticsRelations();
         $form = Form::with($relations)->findOrFail($id);
+        $questionsWithAnswers = collect($this->calculateStatistics($form));
 
-        $pdf = \PDF::loadView('forms.results');
+        $pdf = \PDF::loadView('forms.results', ['diagrams' => $questionsWithAnswers, 'form' => $form]);
+        $pdf->setOption('encoding', 'UTF-8');
         $pdf->setOption('enable-javascript', true);
         $pdf->setOption('javascript-delay', 5000);
         $pdf->setOption('enable-smart-shrinking', true);
