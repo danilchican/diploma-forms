e5a6b43c6991d41484b6221abdf5b4cf0f5cd71e
danilchican <danilchican@mail.ru>
2019-05-11 14:32:58 +0300

    Refactored statistics calculation.

--- app/Http/Controllers/Dashboard/FormController.php
+++ app/Http/Controllers/Dashboard/FormController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Dashboard;
 use App\Exceptions\CreateFormException;
 use App\Exceptions\UpdateFormException;
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\StatisticsTrait;
 use App\Http\Requests\Form\CreateFormRequest;
 use App\Http\Requests\Form\UpdateFormRequest;
 use App\Models\AnswerType;
@@ -21,6 +22,8 @@ use Mavinoo\LaravelBatch\LaravelBatchFacade as Batch;
 
 class FormController extends Controller
 {
+    use StatisticsTrait;
+
     public function showCreateFormPage(Request $request)
     {
         $answerTypes = AnswerType::all();
@@ -67,17 +70,9 @@ class FormController extends Controller
      */
     public function showViewResultsFormPage($id)
     {
-        $prefix = 'answers.submittedAnswers';
-
-        $relations = [
-            $prefix . '.question.answerType',
-            $prefix . '.question' => function ($query) {
-                $query->withCount('answers as answer_variants_count');
-            },
-            $prefix . '.selectedAnswer',
-        ];
-
+        $relations = $this->getStatisticsRelations();
         $form = Form::with($relations)->findOrFail($id);
+
         $questionsWithAnswers = collect($this->calculateStatistics($form));
 
         return view('dashboard.forms.view')->with(compact(['form', 'questionsWithAnswers']));
@@ -299,70 +294,5 @@ class FormController extends Controller
                 $questionModel->answers()->delete();
             }
         }
-    }
-
-    private function calculateStatistics(Form $form)
-    {
-        $statistics = [];
-        $answers = $form->answers->pluck('submittedAnswers')->collapse()->all();
-
-        $questions = collect($answers)->pluck('question')->unique()->all();
-        $answers = collect($answers)->groupBy('question.id');
-
-        foreach ($questions as $question) {
-            $statistics[] = $this->calculateQuestionStatistic($question, $answers->get($question->id));
-        }
-
-        return $statistics;
-    }
-
-    private function calculateQuestionStatistic(FormQuestion $question, $answers)
-    {
-        $diagramType = $this->defineDiagramType($question->answerType, $question->answer_variants_count);
-
-        return [
-            'question_title' => $question->getTitle(),
-            'diagram'        => $diagramType,
-            'answers'        => $this->defineQuestionAnswersStatistic($answers, $diagramType),
-            'total_answers'  => $answers->count(),
-        ];
-    }
-
-    private function defineQuestionAnswersStatistic(Collection $answers, string $diagramType)
-    {
-        $answersCount = $answers->count();
-        $answers = $answers->toArray();
-
-        if ($diagramType === 'list') {
-            return collect($answers)->pluck('text_answer')->all();
-        }
-
-        $needToOrderAnswers = $diagramType === 'horizontal';
-        $statistics = collect();
-
-        $groupedAnswers = collect($answers)->groupBy('selected_answer.title')->all();
-
-        foreach ($groupedAnswers as $answerTitle => $answer) {
-            $statistics->push([
-                'title'      => $answerTitle,
-                'count'      => \count($answer),
-                'percentage' => round(\count($answer) / $answersCount * 100, 2),
-            ]);
-        }
-
-        return $needToOrderAnswers ? $statistics->sortByDesc('count')->all() : $statistics->all();
-    }
-
-    private function defineDiagramType(AnswerType $answerType, $answerVariantsCount)
-    {
-        if (!$answerType->isAnswersRequired()) {
-            return 'list';
-        }
-
-        if ($answerVariantsCount > 5) {
-            return 'horizontal';
-        }
-
-        return 'pie';
     }
 }
---
+++ app/Http/Controllers/StatisticsTrait.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AnswerType;
+use App\Models\Form;
+use App\Models\FormQuestion;
+use Illuminate\Support\Collection;
+
+trait StatisticsTrait
+{
+    protected $statisticsPrefix = 'answers.submittedAnswers';
+
+    protected function getStatisticsRelations()
+    {
+        return [
+            $this->statisticsPrefix . '.question.answerType',
+            $this->statisticsPrefix . '.question' => function ($query) {
+                $query->withCount('answers as answer_variants_count');
+            },
+            $this->statisticsPrefix . '.selectedAnswer',
+        ];
+    }
+
+    private function calculateStatistics(Form $form)
+    {
+        $statistics = [];
+        $answers = $form->answers->pluck('submittedAnswers')->collapse()->all();
+
+        $questions = collect($answers)->pluck('question')->unique()->all();
+        $answers = collect($answers)->groupBy('question.id');
+
+        foreach ($questions as $question) {
+            $statistics[] = $this->calculateQuestionStatistic($question, $answers->get($question->id));
+        }
+
+        return $statistics;
+    }
+
+    private function calculateQuestionStatistic(FormQuestion $question, $answers)
+    {
+        $diagramType = $this->defineDiagramType($question->answerType, $question->answer_variants_count);
+
+        return [
+            'question_title' => $question->getTitle(),
+            'diagram'        => $diagramType,
+            'answers'        => $this->defineQuestionAnswersStatistic($answers, $diagramType),
+            'total_answers'  => $answers->count(),
+        ];
+    }
+
+    private function defineQuestionAnswersStatistic(Collection $answers, string $diagramType)
+    {
+        $answersCount = $answers->count();
+        $answers = $answers->toArray();
+
+        if ($diagramType === 'list') {
+            return collect($answers)->pluck('text_answer')->all();
+        }
+
+        $needToOrderAnswers = $diagramType === 'horizontal';
+        $statistics = collect();
+
+        $groupedAnswers = collect($answers)->groupBy('selected_answer.title')->all();
+
+        foreach ($groupedAnswers as $answerTitle => $answer) {
+            $statistics->push([
+                'title'      => $answerTitle,
+                'count'      => \count($answer),
+                'percentage' => round(\count($answer) / $answersCount * 100, 2),
+            ]);
+        }
+
+        return $needToOrderAnswers ? $statistics->sortByDesc('count')->all() : $statistics->all();
+    }
+
+    private function defineDiagramType(AnswerType $answerType, $answerVariantsCount)
+    {
+        if (!$answerType->isAnswersRequired()) {
+            return 'list';
+        }
+
+        if ($answerVariantsCount > 5) {
+            return 'horizontal';
+        }
+
+        return 'pie';
+    }
+
+}
